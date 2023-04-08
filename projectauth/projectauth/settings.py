3433d5f9ed4ec6dc4cfd61dbb011b6dc28dc12b3
arvind2602 <guptaarvind2602@gmail.com>
2023-04-08 19:23:40 +0530

    Final

--- projectauth/projectauth/settings.py
+++ projectauth/projectauth/settings.py
@@ -129,7 +129,7 @@ MESSAGES_TAG={
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER='guptaarvind2602@gmail.com'
-EMAIL_HOST_PASSWORD='fevasinlkrjhqkpd'
+EMAIL_HOST_PASSWORD='**********'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
